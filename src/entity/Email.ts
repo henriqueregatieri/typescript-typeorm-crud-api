6758cabe3393e8b650751fd2cc1fd7ac63e5ea51
Henrique Regatieri <riq.regatieri@gmail.com>
2021-06-11 00:17:49 -0300

    Validations

--- src/entity/Email.ts
+++ src/entity/Email.ts
@@ -1,10 +1,12 @@
 import { Entity, Column, OneToMany } from 'typeorm';
 import { Record } from './Record';
 import { Product } from './Product';
+import { Length } from 'class-validator';
 
 @Entity()
 export class Email extends Record {
   @Column()
+  @Length(3, 50)
   name: string;
 
   @OneToMany((type) => Product, (product) => product.email, {
--- src/entity/Product.ts
+++ src/entity/Product.ts
@@ -1,13 +1,16 @@
 import { Entity, Column, ManyToOne } from 'typeorm';
 import { Record } from './Record';
 import { Email } from './Email';
+import { Length } from 'class-validator';
 
 @Entity()
 export class Product extends Record {
   @Column()
+  @Length(3, 50)
   title: string;
 
   @Column()
+  @Length(3, 500)
   text: string;
 
   @ManyToOne((type) => Email, (email) => email.products, {
